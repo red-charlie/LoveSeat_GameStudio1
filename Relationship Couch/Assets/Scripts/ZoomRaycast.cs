522fda40cbaf2948d27592709e6384f92210fcb4
EmiSchaufeld <33503014+EmiSchaufeld@users.noreply.github.com>
2019-10-21 18:21:34 -0400

    zoom raycast and audio working

---
+++ Relationship Couch/Assets/Scripts/ZoomRaycast.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomRaycast : MonoBehaviour
+{
+    public Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.isTrigger)
+                {
+
+
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+                    Debug.Log("Did Hit");
+
+                    GameObject thingHit = hit.collider.gameObject;
+                    thingHit.GetComponent<AudioSource>().Play();
+
+                    MeshCollider hitAgain = thingHit.GetComponent<MeshCollider>();
+                    hitAgain.isTrigger = false;
+
+
+                    Debug.Log("is playing? " + thingHit.GetComponent<AudioSource>().isPlaying);
+
+                }
+               
+            }
+        }
+
+    }
+}
+
