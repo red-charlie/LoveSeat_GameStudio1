47a4e481f7a820c07ed93d67575ead144df5d633
EmiSchaufeld <33503014+EmiSchaufeld@users.noreply.github.com>
2019-10-25 11:26:04 -0400

    SOUNDS BABY
    listen up fools

---
+++ Relationship Couch/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using System.Collections;
+ 
+public class Footsteps : MonoBehaviour
+{
+    public float stepRate = 0.5f;
+    public float stepCoolDown;
+    public AudioClip footStep;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        stepCoolDown -= Time.deltaTime;
+        if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f)
+        {
+            GetComponent <AudioSource>().pitch = 1f + Random.Range(-0.2f, 0.2f);
+            GetComponent < AudioSource>().PlayOneShot(footStep, 0.9f);
+            stepCoolDown = stepRate;
+        }
+    }
+}
+
+
