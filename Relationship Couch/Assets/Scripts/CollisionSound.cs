daf77e2d7b1839c437c6d291c676631a4af1297d
Charlie Harper <charlottehdesigns@gmail.com>
2019-10-29 12:32:47 -0400

    *Collision Sound Script -added in the missing script

---
+++ Relationship Couch/Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    public GameObject Transition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    private void OnTriggerEnter(Collider other){
+        Transition.SetActive(true);
+     print("WHAT");
+        
+    }
+}
