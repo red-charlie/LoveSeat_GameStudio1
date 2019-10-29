80d7914baa1a98f626c098dcd43a306179e183ba
Charlie Harper <charlottehdesigns@gmail.com>
2019-10-29 16:40:24 -0400

    *changed escape menu

--- Relationship Couch/Assets/Scripts/EscMenu.cs
+++ Relationship Couch/Assets/Scripts/EscMenu.cs
@@ -19,7 +19,7 @@ public class EscMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space")){
+        if(Input.GetKeyDown("escape")){
            if (Paused == true) {
                
                  EscMenu_1.gameObject.SetActive (false);
