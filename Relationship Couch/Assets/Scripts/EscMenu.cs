b9252299b1c4211084a569e1e222a870b8a160ab
Charlie Harper <charlottehdesigns@gmail.com>
2019-10-29 19:18:11 -0400

    *collision fix and escape menu

--- Relationship Couch/Assets/Scripts/EscMenu.cs
+++ Relationship Couch/Assets/Scripts/EscMenu.cs
@@ -19,7 +19,7 @@ public class EscMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("escape")){
+        if(Input.GetKeyDown("tab")){
            if (Paused == true) {
                
                  EscMenu_1.gameObject.SetActive (false);
